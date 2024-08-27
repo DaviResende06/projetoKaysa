8ecaefa456e8a1535d96b44a81835cca14290f37
Davi resende Rodrigues <daviresende844@gmail.com>
2024-08-27 20:20:55 -0300

    muitas mudanças: Carrossel inserido e mudança no layout

--- js/script.js
+++ js/script.js
@@ -1,3 +1,37 @@
+
+
+var TrandingSlider = new Swiper('.tranding-slider', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 2.9,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
 // Seleciona o modal
 var modal = document.getElementById('myModal');
 
